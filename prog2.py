99f854a977710d3556d2e8e8684f1f25dcfdbd51
Arjun Singh <arjunhpes@gmail.com>
2020-08-19 01:59:42 +0530

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,3 @@
+fname = input("Enter your first name?:")
+lname = input("Enter your last name?")
+print(fname[::-1] + " " + lname[::-1])
---
+++ prog3.py
@@ -0,0 +1,5 @@
+import math
+pi = math.pi
+radius = float(12/2)
+volume = (float(4/3)*(pi)*(radius)**3)
+print(volume)
